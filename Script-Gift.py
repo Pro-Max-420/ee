ad031fb6f47789227e6ee10e3fd0a2a6ca548ff8
Pro-Max-420 <121747358+Pro-Max-420@users.noreply.github.com>
2023-01-18 13:14:19 +0600

    Script-Gift.py

--- Script-Gift.py
+++ Script-Gift.py
@@ -1,9 +1,14 @@
 import os,time
-xoss('\033[0;91m\n Checking Your Mobile...?\033[1;37m')
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.0010)
+print('\033[0;91m\n Checking Your Mobile...?\033[1;37m')
 time.sleep(10)
-xoss('\033[0;92m\n Congratulations! Your Device Support This Tools\033[1;37m')
+print('\033[0;92m\n Congratulations! Your Device Support This Tools\033[1;37m')
 time.sleep(5)
-xoss('\033[0;93m\n JOIN MY SCRIPT GIFT GROUP\033[1;37m')
+print('\033[0;93m\n JOIN MY SCRIPT GIFT GROUP\033[1;37m')
 time.sleep(3)
 os.system("xdg-open https://facebook.com/groups/554714119911648/")
 import R4NDOM
